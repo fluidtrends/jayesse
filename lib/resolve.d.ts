5165bd0cc241691a651f401f29569de44c173095
I. Dan Calinescu <idancalinescu@gmail.com>
2021-02-02 12:38:32 +0200

    using carmel js ts

--- lib/resolve.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const ChunkManifest: (chunk: string) => any;
-export declare const ChunkComponent: (chunk: string, id: string, target?: string) => any;
-declare const _default: (target: string) => any;
-export default _default;
--- src/web/main/dom.tsx
+++ src/web/main/dom.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
+import { AppContainer } from 'react-hot-loader'
 
 import { App } from '..'
-import { resolveWeb } from '@carmel/js'
+import { resolveWeb } from '@carmel/js/src'
 import rawAssets from '../../assets'
 
 const basename = '/'
@@ -11,6 +12,12 @@ const props = resolveWeb()
 const locale = props.locale || 'en'
 const assets = rawAssets(basename, locale)
 
-ReactDOM.render(<BrowserRouter basename={basename}>
-    <App {...props} basename={basename} assets={assets}/>
-</BrowserRouter>, document.getElementById('app'))
+const render = () => {
+    ReactDOM.render(<AppContainer>
+        <BrowserRouter basename={basename}>
+            <App {...props} basename={basename} assets={assets}/>
+        </BrowserRouter>
+    </AppContainer>, document.getElementById('app'))
+}
+  
+render()
